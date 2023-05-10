44b208420b980bd72345b57f4117b06be2a76513
Noe-Martinez-Flores <20203tn035@utez.edu.mx>
2023-05-09 19:08:04 -0600

    end promesas1

--- promesas.js
+++ promesas.js
@@ -1,1 +1,23 @@
-console.log("hi")
+function plusOne(number, callback) {
+  if (number >= 7) {
+    callback("Number too big");
+    return;
+  }
+  setTimeout(function () {
+    callback(null,number + 1);
+  }, 2000);
+}
+
+plusOne(5, function (err, valor) {
+  if (err) return console.log(err);
+  console.log(valor);
+  plusOne(valor, function (err, valor2) {
+    if (err) return console.log(err);
+    console.log(valor2);
+
+    plusOne(valor2, function (err, valor3) {
+        if (err) return console.log(err);
+      console.log(valor3);
+    });
+  });
+});
